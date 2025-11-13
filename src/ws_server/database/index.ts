e3ae8958eda349d199a0f4d0c160a647a1dd09c8
fliderman01 <fliderman01@gmail.com>
2025-11-13 13:28:04 +0400

    feat: main db class

---
+++ src/ws_server/database/index.ts
@@ -0,0 +1,21 @@
+import { PlayerManager } from "./playerManager";
+import { RoomManager } from "./roomManager";
+import { GameManager } from "./gameManager";
+
+export class Database {
+  public players: PlayerManager;
+  public rooms: RoomManager;
+  public games: GameManager;
+
+  constructor() {
+    this.players = new PlayerManager();
+    this.rooms = new RoomManager();
+    this.games = new GameManager();
+  }
+
+  reset(): void {
+    this.players = new PlayerManager();
+    this.rooms = new RoomManager();
+    this.games = new GameManager();
+  }
+}

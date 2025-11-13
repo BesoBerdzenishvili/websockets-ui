4754c59b78f1a5b40a074f9c0733c1a1d469c9fb
fliderman01 <fliderman01@gmail.com>
2025-11-13 13:57:43 +0400

    feat: central message handler

---
+++ src/ws_server/handlers/index.ts
@@ -0,0 +1,62 @@
+import { Database } from "../database";
+import { Message } from "../models/types";
+import { handleAddShips } from "./shipHandler";
+import { MESSAGE_TYPES } from "../models/constants";
+import { handleRegistration } from "./playerHandler";
+import { validateMessage } from "../utils/validation";
+import WebSocket, { Server as WebSocketServer } from "ws";
+import { handleAttack, handleRandomAttack } from "./gameHandler";
+import { handleCreateRoom, handleAddUserToRoom } from "./roomHandler";
+
+export function handleMessage(
+  ws: WebSocket,
+  msg: Message,
+  db: Database,
+  wss: WebSocketServer
+): void {
+  if (!validateMessage(msg)) {
+    console.log("Result: Invalid message format");
+    return;
+  }
+
+  console.log("Received command:", msg.type, JSON.stringify(msg.data));
+
+  switch (msg.type) {
+    case MESSAGE_TYPES.REG:
+      handleRegistration(ws, msg, db, wss);
+      break;
+
+    case MESSAGE_TYPES.CREATE_ROOM:
+      handleCreateRoom(ws, msg, db, wss);
+      break;
+
+    case MESSAGE_TYPES.ADD_USER_TO_ROOM:
+      handleAddUserToRoom(ws, msg, db, wss);
+      break;
+
+    case MESSAGE_TYPES.ADD_SHIPS:
+      handleAddShips(ws, msg, db, wss);
+      break;
+
+    case MESSAGE_TYPES.ATTACK:
+      handleAttack(ws, msg, db, wss);
+      break;
+
+    case MESSAGE_TYPES.RANDOM_ATTACK:
+      handleRandomAttack(ws, msg, db, wss);
+      break;
+
+    default:
+      console.log("Result: Unknown command type:", msg.type);
+  }
+}
+
+export { handleRegistration } from "./playerHandler";
+export { handleCreateRoom, handleAddUserToRoom } from "./roomHandler";
+export { handleAddShips } from "./shipHandler";
+export {
+  handleAttack,
+  handleRandomAttack,
+  processAttack,
+  finishGame,
+} from "./gameHandler";
